56c136aa05591e6cccb77f66a4a6c34427adfef2
Myglce <warungbek62@gmail.com>
2025-05-25 13:11:15 +0800

    Update menu.js

--- plugins/Main_Menu/menu.js
+++ plugins/Main_Menu/menu.js
@@ -27,7 +27,7 @@ module.exports = {
         externalAdReply: {
           title: botName,
           body: 'Silakan pilih menu yang tersedia',
-          thumbnailUrl: thumbnail,
+          thumbnailUrl: 'https://files.catbox.moe/wua74d.jpg',
           sourceUrl: 'https://github.com/maoudabi0',
           mediaUrl: 'https://wa.me/6285725892962?text=Beli+Kak',
           mediaType: 1,
